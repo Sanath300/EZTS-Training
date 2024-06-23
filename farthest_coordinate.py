1a3731719e498fc71fb89b8e760c4ba9139425e6
Sanath300 <priyasanath2003@gmail.com>
2024-06-23 21:15:48 +0530

    Add files via upload

---
+++ farthest_coordinate.py
@@ -0,0 +1,32 @@
+""""
+Arduino
+Tom is an Arduino Programmer. He has designed a program to run his robocar on a
+horizontal number line. Initially, the car is parked at: 0.
+Given an array A of N integers which can be A. B. C... the robocar runs as follows as per the
+designed program
+First the robocar moves A units in specified direction(right in case the integer is positive and
+left if the integer is negative).
+Then robocar first moves A units and then B units in a specified direction.
+In the next step, the robocar moves A units. B units, and then C units in a specified direction.
+This process keeps on repeating as per the number of integers in the sequence..
+Your task is to find and retum an integer value, representing the farthest coordinate reached
+by the robocar from the beginning to the end of the process.
+Sample Input:
+1 -2 3 4
+Sample Output:
+6 
+"""
+def farthest_coordinate(arr):
+    current_position = 0
+    farthest_position = 0
+
+    for move in arr:
+        current_position += move
+        if current_position > farthest_position:
+            farthest_position = current_position
+
+    return farthest_position
+
+# Test the function
+arr = [1, -2, 3, 4]
+print(farthest_coordinate(arr))  # Output: 6
